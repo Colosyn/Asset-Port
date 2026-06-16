544a7a43a2e6225d3535a57d68f3c0a61eddc996
Colosyn <colosynstudio@gmail.com>
2026-06-16 20:26:17 +0530

    feat: add confirm/cancel buttons, and validate folder path

--- asset_port/gui_helper.py
+++ asset_port/gui_helper.py
@@ -5,6 +5,9 @@ from asset_port.importer import AssetImporter
 from asset_port.logger import log_pipeline_report
 from asset_port.config import config_loader
 active_widget = None
+preview_widget = None
+last_folder_path = ""
+last_category = None
 TAB_ID = unreal.Name("/Game/Python/Widgets/EUW_AssetPort.EUW_AssetPort_ActiveTab")
 PREIVEW_ID = unreal.Name("/Game/Python/Widgets/EUW_AssetPort_Preview.EUW_AssetPort_Preview_ActiveTab")
 def run_importer():
@@ -70,7 +73,8 @@ def on_cancel_clicked():
     
     
 def on_preview_clicked():
-    preview_widget = None
+    
+    global active_widget,  preview_widget, last_folder_path, last_category
     config = config_loader()
     subsystem = unreal.get_editor_subsystem(unreal.EditorUtilitySubsystem)
     widget_blueprint = unreal.load_asset("/Game/Python/Widgets/EUW_AssetPort_Preview")
@@ -80,6 +84,13 @@ def on_preview_clicked():
     folder_path_text = folder_path_field.get_text()
     folder_path = unreal.TextLibrary.conv_text_to_string(folder_path_text)
     
+    if not folder_path:
+        unreal.EditorDialog.show_message(
+            "Select Directory",
+            "Please select a valid import directory before launching the preview.",
+            unreal.AppMsgType.OK
+        )
+        return
     category_dropdown = active_widget.get_editor_property("Category_Dropdown")
     category_str = category_dropdown.get_selected_option()
     category = None if category_str in ("None", "Auto-Detect") else category_str
@@ -88,6 +99,9 @@ def on_preview_clicked():
     failed_asset_name =[]
            
     if folder_path:
+        last_folder_path = folder_path
+        last_category = category
+        
         importer = AssetImporter()
         groups, report = importer.import_directory(folder_path, category, True)
         
@@ -116,6 +130,12 @@ def on_preview_clicked():
         preview_widget = subsystem.spawn_and_register_tab(widget_blueprint)
         
     if preview_widget:
+        preview_import = preview_widget.get_editor_property("Conform_Import")
+        preview_cancel = preview_widget.get_editor_property("Cancel_preview")
+        
+        preview_import.on_clicked.add_callable(on_preview_import_clicked)
+        preview_cancel.on_clicked.add_callable(on_preview_cancel_clicked)
+        
         preview_widget.set_editor_property("Import_List_Items", import_asset_name)
         preview_widget.set_editor_property("Failed_List_Items", failed_asset_name)
         preview_widget.call_method("RefreshPreviewUI")
@@ -123,3 +143,22 @@ def on_preview_clicked():
         on_cancel_clicked()
         
         
+def  on_preview_import_clicked():
+    global last_folder_path, last_category
+    
+    if last_folder_path:
+        
+        importer = AssetImporter()
+        group , report = importer.import_directory(last_folder_path,last_category, False)
+        
+        log_pipeline_report(report, last_folder_path)
+        
+    on_preview_cancel_clicked()
+    
+
+def on_preview_cancel_clicked():
+    global preview_widget
+    if preview_widget:
+        subsystem = unreal.get_editor_subsystem(unreal.EditorUtilitySubsystem)
+        subsystem.close_tab_by_id(PREIVEW_ID)
+        preview_widget = None
